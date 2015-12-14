58055e0193a3600391156c9faa962dd0855c6df7
Michele Gargiulo <michele.gargiulo@isobar.com>
2015-12-14 16:52:32 +0100

    Added support for custom I18n backends

--- django/conf/global_settings.py
+++ django/conf/global_settings.py
@@ -41,6 +41,10 @@ TIME_ZONE = 'America/Chicago'
 # If you set this to True, Django will use timezone-aware datetimes.
 USE_TZ = False
 
+# The name of the class to use as i18n backend
+I18N_BACKEND_REAL = 'django.utils.translation.I18nRealBackend'
+I18N_BACKEND_NULL = 'django.utils.translation.I18nNullBackend'
+
 # Language code for this installation. All choices can be found here:
 # http://www.i18nguy.com/unicode/language-identifiers.html
 LANGUAGE_CODE = 'en-us'
